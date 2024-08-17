8606145169eb9ff3b66a9fa6ff5f525b9e43de15
dax-westerman <11837526+dax-westerman@users.noreply.github.com>
2024-08-16 20:43:37 -0500

    Added file to test building and removing needed files

--- extract_needed_files.sh
+++ extract_needed_files.sh
@@ -15,19 +15,65 @@ NVIDIA_DRIVER_FOLDER="NVIDIA-Linux-x86_64-${NV_DRIVER_VERSION}"
 NV_DRIVER_FILE="$NVIDIA_DRIVER_FOLDER.run"
 NVIDIA_DRIVER_URL="https://us.download.nvidia.com/tesla/${NV_DRIVER_VERSION}/${NV_DRIVER_FILE}"
 
-# Pull down CUDA install
-if [[ ! -d "$ABS_PATH_CUDA_EXTRACT"]]; then
-    [[ ! -f "$CUDA_NAME" ]] && curl -O "$CUDA_URL"
-    mkdir -p "$CUDA_NAME"
-    sh "./$CUDA_NAME.run" --extract="${ABS_PATH_CUDA_EXTRACT}"
-fi
+show_help()
+{
+    echo ""
+    echo "  extract_needed_files.sh [-g|r]"
+    echo "      options:"
+    echo "      g     get all the installs/drivers"
+    echo "      r     clean all the installs"
+    echo ""
+}
 
-# CUDNN install
-[[ ! -f "$NV_CUDNN_FILE" ]] && curl -O "$NV_CUDNN_URL"
+get_installs()
+{
+    echo "Getting installs"
 
-# DRIVER
-[[ ! -f "$NV_DRIVER_FILE" ]] && curl -O "$NVIDIA_DRIVER_URL"
+    # Pull down CUDA install
+    if [[ ! -d "$ABS_PATH_CUDA_EXTRACT" ]]; then
+        [[ ! -f "$CUDA_NAME" ]] && curl -O "$CUDA_URL" || echo "Already Exists: $CUDA_NAME"
+        mkdir -p "$CUDA_NAME"
+        sh "./$CUDA_NAME.run" --extract="${ABS_PATH_CUDA_EXTRACT}"
+    else
+        echo "Already Exists: $ABS_PATH_CUDA_EXTRACT"
+    fi
 
-# # Extracts the file into NVIDIA-Linux-x86_64-535.129.03
-# [[ ! -d "$NVIDIA_DRIVER_FOLDER" ]] && sh ./NVIDIA-Linux-x86_64-535.129.03.run --extract-only
+    # CUDNN install
+    [[ ! -f "$NV_CUDNN_FILE" ]] && curl -O "$NV_CUDNN_URL" || echo "Already Exists: $NV_CUDNN_FILE" &
+
+    # DRIVER
+    [[ ! -f "$NV_DRIVER_FILE" ]] && curl -O "$NVIDIA_DRIVER_URL" || echo "Already Exists: $NV_DRIVER_FILE" &
+
+    # # Extracts the file into NVIDIA-Linux-x86_64-535.129.03
+    [[ ! -d "$NVIDIA_DRIVER_FOLDER" ]] && sh "./$NV_DRIVER_FILE" --extract-only || echo "Already Exists: $NVIDIA_DRIVER_FOLDER" &
+
+}
+
+clean_installs()
+{
+    printf "Removing the files/directories\n\n"
+
+    [[ -f "$NV_CUDA_FILE" ]] && rm "$NV_CUDA_FILE" && echo "$NV_CUDA_FILE deleted"
+    [[ -f "$NV_CUDNN_FILE" ]] && rm "$NV_CUDNN_FILE" && echo "$NV_CUDNN_FILE deleted"
+    [[ -f "$NV_DRIVER_FILE" ]] && rm "$NV_DRIVER_FILE" && echo "$NV_DRIVER_FILE deleted"
+    [[ -d "$CUDA_NAME" ]] && rm -rf "$CUDA_NAME" && echo "$CUDA_NAME deleted"
+    [[ -d "$NVIDIA_DRIVER_FOLDER" ]] && rm -rf "$NVIDIA_DRIVER_FOLDER" && echo "$NVIDIA_DRIVER_FOLDER deleted"
+    [[ -d "$ABS_PATH_CUDA_EXTRACT" ]] && rm -rf "$ABS_PATH_CUDA_EXTRACT" && echo "$ABS_PATH_CUDA_EXTRACT deleted"
+}
+
+getopts "gr" o
+
+# while getopts "gr" o; do
+case "${o}" in
+    "g")
+        get_installs;;
+    "r")
+        clean_installs;;
+    "?")
+        show_help
+        ;;
+esac  
+# done
+
+
 
